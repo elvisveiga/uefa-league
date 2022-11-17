eac78a26ec06711c3874a1b928a0376904431ea1
Elvis Weslley da Veiga <eveiga@epharma.com.br>
2022-11-16 23:33:14 -0300

    Melhorias layout

--- src/Pages/Clubes.jsx
+++ src/Pages/Clubes.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import ClubeEscudo from '../Components/ClubeEscudo';
+import { Grid } from '@mui/material';
 
 function Clubes() {
+
+    const listaClubes = [
+        { nome: "Paris", paisAbreviacao: "FRA", urlEscudo: "https://img.uefa.com/imgml/TP/teams/logos/64x64/52747.png" },
+        { nome: "Bayern", paisAbreviacao: "GER", urlEscudo: "https://img.uefa.com/imgml/TP/teams/logos/64x64/50037.png" },
+        { nome: "Napoli", paisAbreviacao: "ITA", urlEscudo: "https://img.uefa.com/imgml/TP/teams/logos/64x64/50136.png" },
+        { nome: "Milan", paisAbreviacao: "ITA", urlEscudo: "https://img.uefa.com/imgml/TP/teams/logos/64x64/50058.png" },
+        { nome: "Benfica", paisAbreviacao: "POR", urlEscudo: "https://img.uefa.com/imgml/TP/teams/logos/64x64/50147.png" },
+        { nome: "Chelsea", paisAbreviacao: "ENG", urlEscudo: "https://img.uefa.com/imgml/TP/teams/logos/64x64/52914.png" },
+    ];
+
     return <>
         <h1>Clubes</h1>
+        <Grid container columnSpacing={2}>
+            {listaClubes.map(clube => <Grid item sm={3} md={2} xs={4} xl={2}>
+                <ClubeEscudo
+                    nome={clube.nome}
+                    paisAbreviacao={clube.paisAbreviacao}
+                    urlEscudo={clube.urlEscudo} />
+            </Grid>)}
+        </Grid>
+
     </>;
 }
 
